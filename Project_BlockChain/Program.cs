2d29490c29fe85a580170104d08c9947b68ce452
장지훈 <s5v7@outlook.kr>
2017-11-30 12:34:30 +0900

    Add Package

--- Project_BlockChain/Program.cs
+++ Project_BlockChain/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin;
+using Project_BlockChain.Chapter;
 
 namespace Project_BlockChain {
 	class Program {
 		static void Main(string[] args) {
+			var chapter1 = new Chapter1();
+
+			chapter1.Lesson4();
+
+			Console.WriteLine();
 		}
 	}
 }
